c368d66fc6d7eceac4ab5e229a5537af1fd3f1c8
Saksham Jaiswal <sjais973@gmail.com>
2021-11-05 18:24:57 +0530

    new

--- src/components/intro/Intro.jsx
+++ src/components/intro/Intro.jsx
@@ -1,11 +1,39 @@
-import React from 'react'
-import './intro.scss'
-const Intro = () => {
-    return (
-        <div className='intro' id='intro'>
-        
-        </div>
-    )
-}
+import React from "react";
+import "./intro.scss";
+import { init } from "ityped";
 
-export default Intro
+const Intro = () => {
+  const textRef = React.useRef();
+  React.useEffect(() => {
+    init(textRef.current, {
+      showCursor: true,
+      backDelay: 1500,
+      backSpeed: 60,
+      strings: ["FrontEnd Developer", "Backend Developer", "MERN Stack"],
+    });
+  }, []);
+
+  return (
+    <div className="intro" id="intro">
+      <div className="left">
+        <div className="imgContainer">
+          <img src="assets/TwitterLogo.png" alt="" />
+        </div>
+      </div>
+      <div className="right">
+        <div className="wrapper">
+          <h2>Hi There, I'm</h2>
+          <h1>Saksham Jaiswal</h1>
+          <h3>
+            <span ref={textRef}></span>
+          </h3>
+        </div>
+        <a href="#portfolio">
+          <img src="assets/down.png" alt="" />
+        </a>
+      </div>
+    </div>
+  );
+};
+
+export default Intro;
--- src/components/menu/Menu.jsx
+++ src/components/menu/Menu.jsx
@@ -1,15 +1,15 @@
 import React from "react";
 import "./menu.scss";
 
-function menu() {
+function menu({menuOpen,setMenuOpen}) {
   return (
-    <div className="menu">
+    <div className={"menu "+(menuOpen && 'active')}>
       <ul>
-        <li><a href="#intro">Home</a></li>
-        <li><a href="#portfolio">Portfolio</a></li>
-        <li><a href="#works">Works</a></li>
-        <li><a href="#testimonials">Testimonials</a></li>
-        <li><a href="#contact">Contact</a></li>
+        <li onClick={()=>setMenuOpen(false)}><a href="#intro">Home</a></li>
+        <li onClick={()=>setMenuOpen(false)}><a href="#portfolio">Portfolio</a></li>
+        <li onClick={()=>setMenuOpen(false)}><a href="#works">Works</a></li>
+        <li onClick={()=>setMenuOpen(false)}><a href="#testimonials">Testimonials</a></li>
+        <li onClick={()=>setMenuOpen(false)}><a href="#contact">Contact</a></li>
 
       </ul>
     </div>
