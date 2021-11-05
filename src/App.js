acd43324379a7e10cbf1b7346607ddfaea849da3
Saksham Jaiswal <sjais973@gmail.com>
2021-11-05 17:03:33 +0530

    new

--- src/App.js
+++ src/App.js
@@ -5,12 +5,14 @@ import Portfolio from './components/portfolio/Portfolio'
 import Works from './components/works/Works'
 import Testimonials from './components/testimonials/Testimonials'
 import Contact from './components/contact/Contact'
+import Menu from "./components/menu/Menu";
 import './app.scss'
 function App() {
   const [menuOpen,setMenuOpen]=React.useState(false)
   return (
     <div className="app">
       <Topbar menuOpen={menuOpen} setMenuOpen={setMenuOpen} />
+      <Menu/>
       <div className="sections">
     <Intro/>
     <Portfolio/>
---
+++ src/components/menu/Menu.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+import "./menu.scss";
+
+function menu() {
+  return (
+    <div className="menu">
+      <ul>
+        <li><a href="#intro">Home</a></li>
+        <li><a href="#portfolio">Portfolio</a></li>
+        <li><a href="#works">Works</a></li>
+        <li><a href="#testimonials">Testimonials</a></li>
+        <li><a href="#contact">Contact</a></li>
+
+      </ul>
+    </div>
+  );
+}
+
+export default menu;
