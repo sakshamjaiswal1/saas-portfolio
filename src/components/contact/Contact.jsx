4f7ddf162a2cab42e01ac57bd701d81fea2c5a7e
Saksham Jaiswal <sjais973@gmail.com>
2021-11-05 23:44:03 +0530

    responsive

--- src/components/contact/Contact.jsx
+++ src/components/contact/Contact.jsx
@@ -1,12 +1,28 @@
-import React from 'react'
-import './contact.scss'
+import React from "react";
+import "./contact.scss";
 
 function Contact() {
-    return (
-        <div className='contact'>
-            
-        </div>
-    )
+    const [message,setMessage]=React.useState(false)
+  const handleSubmit=(e)=>{
+    e.preventDefault()
+    setMessage(true)
+  }  
+  return (
+    <div className="contact" id="contact">
+      <div className="left">
+          <img src="assets/shake.svg" alt="" />
+      </div>
+      <div className="right">
+          <h2>Contact Me</h2>
+          <form onSubmit={handleSubmit} >
+              <input type="email" placeholder="Email" />
+              <textarea  placeholder='Message'></textarea>
+              <button type='submit'>Send</button>
+              {message && <span>Thnks for contacting,I'll reply ASAP </span>}
+          </form>
+      </div>
+    </div>
+  );
 }
 
-export default Contact
+export default Contact;
